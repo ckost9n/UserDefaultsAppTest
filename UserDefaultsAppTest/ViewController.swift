d635d8fe7e69c8bab171e75f3cbad6a2965d7526
ckost9n <ckost9n@gmail.com>
2021-09-16 15:35:21 +0300

    first setup view and alert

--- UserDefaultsAppTest/ViewController.swift
+++ UserDefaultsAppTest/ViewController.swift
@@ -9,11 +9,34 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var userNameLabel: UILabel!
+    
+    @IBOutlet var firstTextLabel: UITextField!
+    @IBOutlet var secondTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        userNameLabel.isHidden = true
     }
 
-
+    @IBAction func donePressed(_ sender: Any) {
+        
+        
+    }
+    
 }
 
+extension ViewController {
+    
+    private func wrongFormatAlert() {
+        
+        let alert = UIAlertController(title: "Wrong Foramt!",
+                                      message: "Please enter your name",
+                                      preferredStyle: .alert)
+        
+        let okActin = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(okActin)
+        present(alert, animated: true, completion: nil)
+    }
+}
