179f064c3ad986db25535e3d32d7b8aeeda29f5c
ckost9n <ckost9n@gmail.com>
2021-09-16 16:20:28 +0300

    Save to UserDefaults and restore from UserDefaults

--- UserDefaultsAppTest/ViewController.swift
+++ UserDefaultsAppTest/ViewController.swift
@@ -16,15 +16,41 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         userNameLabel.isHidden = true
+        
+        if let userName = UserDefaults.standard.value(forKey: "userName")  {
+            userNameLabel.isHidden = false
+            userNameLabel.text = userName as? String
+        }
+        
+        
     }
 
     @IBAction func donePressed(_ sender: Any) {
         
+        guard let firstName = firstTextLabel.text, !firstName.isEmpty else {
+            wrongFormatAlert()
+            return
+        }
+        guard let secondName = secondTextField.text, !secondName.isEmpty else {
+            wrongFormatAlert()
+            return
+        }
         
+        if let _ = Double(firstName) {
+            wrongFormatAlert()
+        } else if let _ = Double(secondName) {
+            wrongFormatAlert()
+        } else {
+            
+            userNameLabel.isHidden = false
+            userNameLabel.text = firstName + " " + secondName
+            UserDefaults.standard.setValue(userNameLabel.text, forKey: "userName")
+        }
+        
+        firstTextLabel.text = nil
+        secondTextField.text = nil
     }
-    
 }
 
 extension ViewController {
@@ -32,7 +58,7 @@ extension ViewController {
     private func wrongFormatAlert() {
         
         let alert = UIAlertController(title: "Wrong Foramt!",
-                                      message: "Please enter your name",
+                                      message: "Please enter your name or surname",
                                       preferredStyle: .alert)
         
         let okActin = UIAlertAction(title: "OK", style: .default, handler: nil)
